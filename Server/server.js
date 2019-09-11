8e240821e0fb95af6aac7b690eaeb2c61433d8fd
shyamkondisetty <shyamkondisetty@gmail.com>
2019-09-12 02:54:42 +0530

    Add. connection database for project

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,16 @@
 const express=require('express');
 const app=express();
+const mongoose =require('mongoose')
+const dbConfig = require('./config/database.config');
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("database coconnected to project");    
+}).catch(err => {
+    console.log('failed to connect to database', err);
+    process.exit();
+});
 app.listen(3000, () => {
     console.log('server is running on ',3000)
 });
