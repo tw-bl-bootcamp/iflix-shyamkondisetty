93a6302a37b8f4c8843ee8e77d6326467d27946a
shyamkondisetty <shyamkondisetty@gmail.com>
2019-09-12 20:52:00 +0530

    Add. user can login with valid credentials

---
+++ Server/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    userEmail: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, {
+    timestamps: true
+}
+);
+
+const user = mongoose.model('users', userSchema);
+
+function User() { }
+User.prototype.login = (loginData, callback) => {
+    user.findOne({ userEmail: loginData.userEmail, password: loginData.password }, (err, userResult) => {
+        if (err) {
+            callback(err)
+        }
+        else {
+            callback(null, userResult)
+        }
+
+    })
+}
+module.exports = new User();
